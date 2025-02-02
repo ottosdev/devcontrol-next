d0dce10aafc3295d801bed2ae02f23e86dd76192
ottoneto <ottosouza.dev@gmail.com>
2025-02-01 23:06:50 -0300

    lint

--- src/app/api/ticket/route.ts
+++ src/app/api/ticket/route.ts
@@ -7,38 +7,35 @@ export async function PATCH(req: Request) {
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
-    return { status: 401, body: "Unauthorized" };
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
   const { id } = await req.json();
 
   const findTicket = await prisma.ticket.findUnique({
-    where: {
-      id: id as string,
-    },
+    where: { id: id as string },
   });
 
   if (!findTicket) {
-    return { status: 404, body: "Ticket not found" };
+    return NextResponse.json({ message: "Ticket not found" }, { status: 404 });
   }
 
   try {
-    await prisma.ticket.update({
-      where: {
-        id: id as string,
-      },
-      data: {
-        status: "FECHADO",
-      },
+    const updatedTicket = await prisma.ticket.update({
+      where: { id: id as string },
+      data: { status: "FECHADO" },
     });
 
     return NextResponse.json(
-      { message: `Ticket ${findTicket.name} updated`, ticket: findTicket },
+      {
+        message: `Ticket ${updatedTicket.name} updated`,
+        ticket: updatedTicket,
+      },
       { status: 200 }
     );
   } catch (error) {
     return NextResponse.json(
-      { message: "Error update ticket" + error },
+      { message: "Error updating ticket", error: error },
       { status: 400 }
     );
   }
