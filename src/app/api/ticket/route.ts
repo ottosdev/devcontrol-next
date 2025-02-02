ac9175950c480429e6c4352ae489a168aa73cd06
ottoneto <ottosouza.dev@gmail.com>
2025-02-02 00:02:46 -0300

    teste

--- src/app/api/ticket/route.ts
+++ src/app/api/ticket/route.ts
@@ -1,39 +1,40 @@
-import { authOptions } from "@/lib/auth";
-import prisma from "@/lib/prisma";
-import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import prismaClient from "@/lib/prisma";
 
-export async function PATCH(req: Request) {
+// http://localhost:3000/api/ticket
+export async function PATCH(request: Request) {
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    return NextResponse.json({ error: "Not authorized" }, { status: 401 });
   }
 
-  const { id } = await req.json();
+  const { id } = await request.json();
 
-  const findTicket = await prisma.ticket.findUnique({
-    where: { id: id as string },
+  const findTicket = await prismaClient.ticket.findFirst({
+    where: {
+      id: id as string,
+    },
   });
 
   if (!findTicket) {
-    return NextResponse.json({ message: "Ticket not found" }, { status: 404 });
+    return NextResponse.json({ error: "Filed update ticket" }, { status: 400 });
   }
 
   try {
-    await prisma.ticket.update({
-      where: { id: id as string },
-      data: { status: "FECHADO" },
+    await prismaClient.ticket.update({
+      where: {
+        id: id as string,
+      },
+      data: {
+        status: "FECHADO",
+      },
     });
 
-    return NextResponse.json(
-      { message: "Ticket updated successfully" },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Error updating ticket", error: error },
-      { status: 400 }
-    );
+    return NextResponse.json({ message: "Chamado atualizado com sucesso!" });
+  } catch (err) {
+    return NextResponse.json({ error: "Filed update ticket" }, { status: 400 });
   }
 }
